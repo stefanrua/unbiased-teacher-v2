74cf9d9916cef615980fb985744df19cd639341d
Stefan Rua <stefan.rua@iki.fi>
2022-12-16 11:39:25 +0200

    Try FCOS

---
+++ scripts/fcos-train.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=barley-fcos-train
+#SBATCH --account=project_2005430
+#SBATCH --partition=gpumedium
+#SBATCH --time=24:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --gres=gpu:a100:4,nvme:50
+
+module load pytorch
+tar -xvf /scratch/project_2005430/ruastefa/datasets/alien-barley-0.0.1.tar -C $LOCAL_SCRATCH
+
+srun python3 train_net.py \
+      --num-gpus 4 \
+      --config configs/FCOS/alien-barley/all_samples.yaml
